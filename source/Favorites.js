e783f77f1002939d2ecabe07390562b15288f9cc
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-04-27 14:38:43 -0300

    add github api conection

--- source/Favorites.js
+++ source/Favorites.js
@@ -1,7 +1,24 @@
+export class githubUser {
+    static search(username) {
+        const endpoint = `https://api.github.com/users/${username}`
+
+        return fetch(endpoint)
+            .then(data => data.json())
+            .then(({ login, name, public_repos, followers }) => ({
+                login,
+                name,
+                public_repos,
+                followers
+            }))
+    }
+}
+
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
         this.load()
+
+
     }
 
     load() {
